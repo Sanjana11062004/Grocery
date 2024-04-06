3975cc8d4959f2942c6afd41c7ade2734018086e
Sanjana M <sanjanamurugesan06@gmail.com>
2024-04-06 11:47:31 +0530

    .

--- assets/js.js
+++ assets/js.js
@@ -1,19 +1,4 @@
-// document.write("hii");
-// let p= fetch('country.json')
-// p.then((value1)=>{
-//     console.log(value1);
-//     console.log(value1.status)
-//     console.log(value1.ok)
-//     return value1.json();
-// })
-// .then((value2)=>{
-//     console.log(value2);
-//     value2.forEach(elemet => {
-//         const vari=  `<li>${elemet.country_id}</li>`;
-//         document.querySelector("ul").insertAdjacentHTML("beforeend",vari);
-//     });
-// })
-// .catch(error => console.log(error));
+
 
 let inputItems = [];
 
@@ -64,3 +49,21 @@ let inputItems = [];
                 displayInput(item);
             });
         });
+
+
+document.write("hii");
+let p= fetch('country.json')
+p.then((value1)=>{
+    console.log(value1);
+    console.log(value1.status)
+    console.log(value1.ok)
+    return value1.json();
+})
+.then((value2)=>{
+    console.log(value2);
+    value2.forEach(elemet => {
+        const vari=  `<li>${elemet.country_id}</li>`;
+        document.querySelector("ul").insertAdjacentHTML("beforeend",vari);
+    });
+})
+.catch(error => console.log(error));
